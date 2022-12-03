a7ffeefd3c1af1c0aa88575c0a48680921ab28c1
Naetan07 <oreofeogunrekun@gmail.com>
2022-12-03 13:25:04 +0100

    done error and loading

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
 
 const Error = () => (
-  <div>Error</div>
+  <div className="w-ful flex justify-center items-center">
+     <h1 className='font-bold text-2xl mt-2 text-white'>Something went wrong please try again</h1>
+  </div>
 );
 
 export default Error;
