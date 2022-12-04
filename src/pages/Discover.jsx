968082d24a5f557e8c4aae14de95f94fc94c5b7d
Naetan07 <oreofeogunrekun@gmail.com>
2022-12-04 10:57:26 +0100

    .

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,17 @@
-import { Error, Loader, SongCard } from "../components";
+import { useDispatch, useSelector } from 'react-redux';
+
+import { Error, Loader, MusicPlayer, SongCard } from "../components";
 import { genres } from '../assets/constants';
 
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
+
 const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+
+
     const { data, isFetching, error } = useGetTopChartsQuery();
 
     const genreTitle ='Pop'
@@ -30,6 +38,9 @@ const Discover = () => {
                     <SongCard
                     key={song.key}
                     song={song}
+                    isPlaying={isPlaying}
+                    activeSong={activeSong}
+                    data={data}
                     i={i}
                     />
                 ))}
