29cf26bcd9874717a03288b761fe2f774621cbfe
Naetan07 <oreofeogunrekun@gmail.com>
2022-12-06 20:29:04 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,8 @@ const App = () => {
         </div>
       </div>
 
+      
+            
       {activeSong?.title && (
         <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#c4dbe0] backdrop-blur-lg rounded-t-3xl z-10">
           <MusicPlayer />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,14 +8,14 @@ import { links } from '../assets/constants'
 const NavLinks = ({handleClick}) => (
   <div className='mt-10'>
     {links.map((item) => (
-      <navLink 
+      <NavLink 
       key={item.name}
       to={item.to}
       className = "flex flex-row justify-start items-center my-8 font-medium text-gray-400 hover:text-cyn-400"
        onClick={() => handleClick && handleClick()}>
         <item.icon className='w-6 h-6 mr-3'/>
         {item.name}
-      </navLink>
+      </NavLink>
     ))}
   </div>
 );
@@ -28,13 +28,13 @@ return(
   <>
   <div className = "md:flex hidden flex-col w-[240px] py-10 px-4 bg-gradient-to-br from-black to-[#c4dbe0] backdrop-blur-sm">
     <img src={logo} alt="logo" className='w-full h-14 object-contain'/>
-    <NavLinks />
+    <NavLinks/>
   </div>
 
   <div className='absolute md:hidden block top-6 right-3'>
     {mobileMenuOpen ? (
       <RiCloseLine 
-      className='w-6 h-6 text-white mr-2' onClick={() => setMobileMenuOpen(false)}/> ) : 
+      className='w-6 h-6 text-white mr-2' onClick={() => setMobileMenuOpen(false)}/> ) :  
       <HiOutlineMenu 
       className='w-6 h-6 text-white mr-2' onClick={() => setMobileMenuOpen(true)}/>
     }
@@ -42,7 +42,7 @@ return(
   <div className = {`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#202222] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition
    ${mobileMenuOpen ? 'left-0' : '-left-full' }`}>
     <img src={logo} alt="logo" className='w-full h-14 object-contain'/>
-    <NavLinks  handleClick={() => setMobileMenuOpen(false)} />
+    <NavLinks  handleClick={() => setMobileMenuOpen(false)}/>
   </div>
   </>
 )
